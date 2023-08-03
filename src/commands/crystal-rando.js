72c02a473c5019613b2aeb2c8e6e4ca0ac629f7d
asazas <asazas0@hotmail.com>
2023-08-03 13:29:49 +0200

    nombres de seeds de crystal rando

--- src/commands/crystal-rando.js
+++ src/commands/crystal-rando.js
@@ -16,6 +16,9 @@ module.exports = {
 				.addChoices({ name: 'Extreme Full Item Randomizer', value: 'TourneySettings/FullItemRandoTournament2023Stage1Extreme.yml' },
 					{ name: 'Crazy Full Item Randomizer', value: 'TourneySettings/FullItemRandoTournament2023Stage2Crazy.yml' },
 					{ name: 'Key Item Randomizer', value: 'TourneySettings/TorneoExtremeKIR2023.yml' }))
+		.addStringOption(option =>
+			option.setName('nombre')
+				.setDescription('Nombre de la seed.'))
 		.addBooleanOption(option =>
 			option.setName('spoiler')
 				.setDescription('Habilitar si se desea generar los logs de spoiler de la seed.')),
@@ -27,8 +30,10 @@ module.exports = {
 		const modo = interaction.options.getString('modo');
 		const spoiler = interaction.options.getBoolean('spoiler');
 		const spoiler_str = spoiler ? '-l' : '';
-
-		const rom_name = `${random_words[Math.floor(Math.random() * random_words.length)]}${random_words[Math.floor(Math.random() * random_words.length)]}`;
+		let rom_name = interaction.options.getString('nombre');
+		if (!rom_name) {
+			rom_name = `${random_words[Math.floor(Math.random() * random_words.length)]}${random_words[Math.floor(Math.random() * random_words.length)]}`;
+		}
 
 		const tmp_dir = await exec('mktemp -d');
 		const tmp_dir_name = tmp_dir['stdout'].trim();
@@ -40,9 +45,11 @@ module.exports = {
 
 
 		if (spoiler) {
-			await interaction.editReply({ files: [`${tmp_dir_name}/${rom_name}.gbc`,
-				{ attachment: `${tmp_dir_name}/${rom_name}_UPR.gbc.log`, name: `SPOILER_${tmp_dir_name}/${rom_name}_UPR.gbc.log` },
-				{ attachment: `${tmp_dir_name}/${rom_name}.gbc_SPOILER.txt`, name: `SPOILER_${tmp_dir_name}/${rom_name}_UPR.gbc.log` }] });
+			await interaction.editReply({
+				files: [`${tmp_dir_name}/${rom_name}.gbc`,
+					{ attachment: `${tmp_dir_name}/${rom_name}_UPR.gbc.log`, name: `SPOILER_${tmp_dir_name}/${rom_name}_UPR.gbc.log` },
+					{ attachment: `${tmp_dir_name}/${rom_name}.gbc_SPOILER.txt`, name: `SPOILER_${tmp_dir_name}/${rom_name}_UPR.gbc.log` }],
+			});
 		}
 		else {
 			await interaction.editReply({ files: [`${tmp_dir_name}/${rom_name}.gbc`] });
