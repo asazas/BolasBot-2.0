cf10161fb405701465ef8896f383cb26fe33e78c
asazas <asazas0@hotmail.com>
2023-08-05 01:08:38 +0200

    refactorización de crystal rando, soporte bingo

--- src/commands/crystal-rando.js
+++ src/commands/crystal-rando.js
@@ -5,6 +5,77 @@ const { random_words } = require('../racing/async_util');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
+
+/**
+ * @summary Llamado al solicitar la generación de una seed de Pokémon Crystal Randomizer (no item randomizer).
+ *
+ * @description Genera una seed de Pokémon Crystal Randomizer y la envía como respuesta al comando.
+ *
+ * @param {CommandInteraction} interaction    Interacción correspondiente al comando inicialmente invocado.
+ * @param {string}             modo_upr       Ruta relativa al preset de Universal Pokémon Randomizer.
+ * @param {string}             nombre         Nombre que se asignará a la seed generada.
+ * @param {string}             spoiler        Opción de línea de comandos para habilitar (o no) la generación del log
+ * 											  de spoiler de la seed.
+ */
+async function generate_upr(interaction, modo_upr, nombre, spoiler) {
+	const tmp_dir = await exec('mktemp -d');
+	const tmp_dir_name = tmp_dir['stdout'].trim();
+
+	await exec(`java -jar universal-pokemon-randomizer-zx.jar cli -s ZXsettings/${modo_upr} -i crystal-speedchoice-8.gbc -o "${tmp_dir_name}/${nombre}.gbc" ${spoiler}`,
+		{ cwd: UPRPath });
+
+
+	if (spoiler) {
+		await interaction.editReply({
+			files: [`${tmp_dir_name}/${nombre}.gbc`,
+				{ attachment: `${tmp_dir_name}/${nombre}.gbc.log`, name: `SPOILER_${tmp_dir_name}/${nombre}.gbc.log` }],
+		});
+	}
+	else {
+		await interaction.editReply({ files: [`${tmp_dir_name}/${nombre}.gbc`] });
+	}
+
+	await exec(`rm -rf ${tmp_dir_name}`);
+}
+
+
+/**
+ * @summary Llamado al solicitar la generación de una seed de Pokémon Crystal Item Randomizer.
+ *
+ * @description Genera una seed de Pokémon Crystal Item Randomizer y la envía como respuesta al comando.
+ *
+ * @param {CommandInteraction} interaction    Interacción correspondiente al comando inicialmente invocado.
+ * @param {string}             modo_upr       Ruta relativa al preset de Universal Pokémon Randomizer.
+ * @param {string}             modo_itemrando Ruta relativa al preset de Pokémon Crystal Item Randomizer.
+ * @param {string}             nombre         Nombre que se asignará a la seed generada.
+ * @param {string}             spoiler        Opción de línea de comandos para habilitar (o no) la generación de logs
+ * 											  de spoiler de la seed.
+ */
+async function generate_itemrando(interaction, modo_upr, modo_itemrando, nombre, spoiler) {
+	const tmp_dir = await exec('mktemp -d');
+	const tmp_dir_name = tmp_dir['stdout'].trim();
+
+	await exec(`java -jar universal-pokemon-randomizer-zx.jar cli -s ZXsettings/${modo_upr} -i crystal-speedchoice-8.gbc -o "${tmp_dir_name}/${nombre}_UPR.gbc" ${spoiler}`,
+		{ cwd: UPRPath });
+	await exec(`"./Pokemon Crystal Item Randomizer" cli -i "${tmp_dir_name}/${nombre}_UPR.gbc" -o "${tmp_dir_name}/${nombre}.gbc" -m "./Modes/${modo_itemrando}" ${spoiler}`,
+		{ cwd: ItemRandoPath });
+
+
+	if (spoiler) {
+		await interaction.editReply({
+			files: [`${tmp_dir_name}/${nombre}.gbc`,
+				{ attachment: `${tmp_dir_name}/${nombre}_UPR.gbc.log`, name: `SPOILER_${tmp_dir_name}/${nombre}_UPR.gbc.log` },
+				{ attachment: `${tmp_dir_name}/${nombre}.gbc_SPOILER.txt`, name: `SPOILER_${tmp_dir_name}/${nombre}_UPR.gbc.log` }],
+		});
+	}
+	else {
+		await interaction.editReply({ files: [`${tmp_dir_name}/${nombre}.gbc`] });
+	}
+
+	await exec(`rm -rf ${tmp_dir_name}`);
+}
+
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('crystal-rando')
@@ -15,7 +86,8 @@ module.exports = {
 				.setRequired(true)
 				.addChoices({ name: 'Extreme Full Item Randomizer', value: 'TourneySettings/FullItemRandoTournament2023Stage1Extreme.yml' },
 					{ name: 'Crazy Full Item Randomizer', value: 'TourneySettings/FullItemRandoTournament2023Stage2Crazy.yml' },
-					{ name: 'Key Item Randomizer', value: 'TourneySettings/TorneoExtremeKIR2023.yml' }))
+					{ name: 'Key Item Randomizer', value: 'TourneySettings/TorneoExtremeKIR2023.yml' },
+					{ name: 'Bingo', value: 'Crystal_Bingo_FullTM_RandomTradeItems.rnqs' }))
 		.addStringOption(option =>
 			option.setName('nombre')
 				.setDescription('Nombre de la seed.'))
@@ -36,26 +108,11 @@ module.exports = {
 		}
 		rom_name = rom_name.substring(0, 50);
 
-		const tmp_dir = await exec('mktemp -d');
-		const tmp_dir_name = tmp_dir['stdout'].trim();
-
-		await exec(`java -jar universal-pokemon-randomizer-zx.jar cli -s ZXsettings/standard.rnqs -i crystal-speedchoice-8.gbc -o "${tmp_dir_name}/${rom_name}_UPR.gbc" ${spoiler_str}`,
-			{ cwd: UPRPath });
-		await exec(`"./Pokemon Crystal Item Randomizer" cli -i "${tmp_dir_name}/${rom_name}_UPR.gbc" -o "${tmp_dir_name}/${rom_name}.gbc" -m "./Modes/${modo}" ${spoiler_str}`,
-			{ cwd: ItemRandoPath });
-
-
-		if (spoiler) {
-			await interaction.editReply({
-				files: [`${tmp_dir_name}/${rom_name}.gbc`,
-					{ attachment: `${tmp_dir_name}/${rom_name}_UPR.gbc.log`, name: `SPOILER_${tmp_dir_name}/${rom_name}_UPR.gbc.log` },
-					{ attachment: `${tmp_dir_name}/${rom_name}.gbc_SPOILER.txt`, name: `SPOILER_${tmp_dir_name}/${rom_name}_UPR.gbc.log` }],
-			});
+		if (modo.endsWith('.rnqs')) {
+			await generate_upr(interaction, modo, rom_name, spoiler_str);
 		}
 		else {
-			await interaction.editReply({ files: [`${tmp_dir_name}/${rom_name}.gbc`] });
+			await generate_itemrando(interaction, 'standard.rnqs', modo, rom_name, spoiler_str);
 		}
-
-		await exec(`rm -rf ${tmp_dir_name}`);
 	},
 };
